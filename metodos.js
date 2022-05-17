153cd0786feabd0f473a5eb90a7819ff3a9c8076
Cristian Tonato <cristianotona@gmail.com>
2022-05-17 23:22:22 +0200

    initial
    Signed-off-by: Cristian Tonato <cristianotona@gmail.com>

--- metodos.js
+++ metodos.js
@@ -1,22 +1,12 @@
-//for each. permite iterar contenido de array.
-
-//sort: ordena elementos de array
-
-// concat: une arrays
-
-//fill:cambia los elementos por valor estatico. 
-
-// slice: devuelve una copia de una parte del array.
-
-//reverse: inverte elementos de array.
-
-//splice: cambia contenido agregando o elimando elementos
-
-//lastIndexof: busca un elemento en el array y dev su posicion. comienza 
-//buscando desde final
-
-//flat: dev array de subarray.
-
-//isArray: determina si el valor es un array.
-
-//from:crear una nueva array apartir de un objeto-
+export function arrayLenght(array){
+    return array.lenght
+};
+export function arrayPush(array){
+    array[array.lenght]= item;
+    return array.lenght
+}
+export function pop(array) {
+    let item = array[array.length - 1];
+    array.length = array.length - 1;
+    return item;
+}
